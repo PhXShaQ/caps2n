4409c9ee65d2b74db9745c3beccb349de06140a0
kevin Bacsal <kevinbacsal9@gmail.com>
2026-04-07 21:48:30 +0800

    database

--- pages/config.php
+++ pages/config.php
@@ -1,13 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "kevins_angels_db";
+$host = "localhost"; // Madalas localhost pa rin sa Hostinger
+$db_user = "u646717549_kevinsangel"; // Yung galing sa MySQL Databases menu
+$db_pass = "Kevs09219172474"; 
+$db_name = "u646717549_kevin";
 
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($host, $db_user, $db_pass, $db_name);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
