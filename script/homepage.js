7e4d7abbaf5e0aa3b61bfa11f0fadd1260b9d55d
kevin Bacsal <kevinbacsal9@gmail.com>
2026-04-13 23:13:21 +0800

    asd

--- script/homepage.js
+++ script/homepage.js
@@ -1,18 +1,19 @@
 
 lucide.createIcons();
+
 function toggleAccountModal() {
     const modal = document.getElementById('accountModal');
     modal.classList.toggle('hidden');
 }
 
-// Close when clicking outside
-window.onclick = function(event) {
+window.addEventListener("click", function(event) {
     const modal = document.getElementById('accountModal');
     const trigger = document.querySelector('.profile-trigger');
+
     if (!modal.contains(event.target) && !trigger.contains(event.target)) {
         modal.classList.add('hidden');
     }
-}
+});
 
 anime.timeline({loop: true})
   .add({
