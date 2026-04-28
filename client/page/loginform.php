40a399664beb153bb24f41b5925379aa35073ad6
kevin Bacsal <kevinbacsal9@gmail.com>
2026-04-28 13:31:57 +0800

    ty

--- client/page/loginform.php
+++ client/page/loginform.php
@@ -68,7 +68,7 @@ $conn->close();
 
     <div class="divider"></div>
 
-    <form method="POST" action="loginform.php">
+    <form method="POST" action="loginform.php"> 
         <div class="input-box">
             <input type="email" name="email" class="input" placeholder=" " required id="email">
             <label>Email</label>
